0a7b6ff5b79d5ddd7e9311951ca9def0a95a6649
朱少鹏 <cangcang333@163.com>
2018-12-05 10:54:53 -0800

    add some function to mm.c

--- vm/malloc/mm.c
+++ vm/malloc/mm.c
@@ -24,3 +24,139 @@
 /* Given block ptr bp, compute address of next and previous blocks */
 #define NEXT_BLKP(bp)  ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
 #define PREV_BLKP(bp)  ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
+
+
+int mm_init(void)
+{
+	/* Create the initial empty heap */
+	if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *)-1)
+	{
+		return -1;
+	}
+	PUT(heap_listp, 0);                               /* Alignment padding */
+	PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));    /* Prologue header */
+	PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));    /* prologue footer */
+	PUT(heap_listp + (3 * WSIZE), PACK(0, 1));        /* Epilogue header */
+	heap_listp += (2 * WSIZE);
+
+	/* Extend the empty heap with a free block of CHUNKSIZE bytes */
+	if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
+	{
+		return -1;
+	}
+
+	return 0;
+}
+
+static void *extend_heap(size_t words)
+{
+	char *bp;
+	size_t size;
+
+	/* Allocate an even number of words to maintain alignment */
+	size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+	if ((long)(bp = mem_sbrk(size)) == -1)
+	{
+		return NULL;
+	}
+
+	/* Initialize free block header/footer and the epilogue header */
+	PUT(HDRP(bp), PACK(size, 0));             /* Free block header */
+	PUT(FTRP(bp), PACK(size, 0));             /* Free block footer */
+	PUT(HDRP(NEXT_BLKP(bp)), PACK(size, 0));  /* New epilogue header */
+
+	/* Coalesce if the previous block was free */
+	return coalesce(bp);
+}
+
+void mm_free(void *bp)
+{
+	size_t size = GET_SIZE(HDRP(bp));
+
+	PUT(HDRP(bp), PACK(size, 0));
+	PUT(FTRP(bp), PACK(size, 0));
+	coalesce(bp);
+}
+
+static void *coalesce(void *bp)
+{
+	size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+	size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+	size_t size = GET_SIZE(HDRP(bp));
+
+	if (prev_alloc && next_alloc)
+	{
+		return bp;
+	}
+	else if (prev_alloc && !next_alloc)
+	{
+		size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+		PUT(HDRP(bp), PACK(size, 0));
+		PUT(FTRP(bp), PACK(size, 0));
+		
+	}
+	else if (!prev_alloc && next_alloc)
+	{
+		size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+		PUT(FTRP(bp), PACK(size, 0));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+		bp = PREV_BLKP(bp);
+	}
+	else 
+	{
+		size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+		PUT(FTRP(PREV_BLKP(bp)), PACK(size, 0));
+		bp = PREV_BLKP(bp);
+	}
+
+	return bp;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
