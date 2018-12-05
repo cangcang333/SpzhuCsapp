6cce0e30d3b7adaba9e683e44e58a94a8e665721
朱少鹏 <cangcang333@163.com>
2018-12-05 04:29:28 -0800

    finish mm.c

--- vm/malloc/mm.c
+++ vm/malloc/mm.c
@@ -7,4 +7,20 @@
 #define MAX(x, y)  ((x) > (y) ? (x) : (y))
 
 /* Pack a size and allocated bit into a word */
+#define PACK(size, alloc)  ((size) | (alloc))
 
+/* Read and write a word at address p */
+#define GET(p)      (*(unsigned int *)(p))
+#define PUT(p, val) (*(unsigned int *)(p) = (val))
+
+/* Read the size and allocted fields from address p */
+#define GET_SIZE(p)   (GET(p) & ~0x7)
+#define GET_ALLOC(p)  (GET(p) & 0x1)
+
+/* Given block ptr bp, compute address of its header and footer */
+#define HDRP(bp)  ((char *)(bp) - WSIZE)
+#define FTRP(bp)  ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+/* Given block ptr bp, compute address of next and previous blocks */
+#define NEXT_BLKP(bp)  ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp)  ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
