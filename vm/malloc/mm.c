5180b87b3c34005913cc31a3338eac7682559834
朱少鹏 <cangcang333@163.com>
2018-12-05 19:21:52 -0800

    add mm_malloc function

--- vm/malloc/mm.c
+++ vm/malloc/mm.c
@@ -114,7 +114,45 @@ static void *coalesce(void *bp)
 	return bp;
 }
 
+void *mm_malloc(size_t size)
+{
+	size_t asize;       /* Adjusted block size */
+	size_t extendsize;  /* Amount to extend heap if no fit */
+	char *bp;
 
+	/* Ignore spurious requests */
+	if (size == 0)
+	{
+		return NULL;
+	}
+
+	/* Adjust block size to include overhead and alignment reqs */
+	if (size <= DSIZE)
+	{
+		asize = 2 * DSIZE;
+	}
+	else
+	{
+		asize = DSIZE * ((size + (DSIZE) + (DSIZE - 1)) / DSIZE);
+	}
+
+	/* Search the free list for a fit */
+	if ((bp = find_fit(asize)) != NULL)
+	{
+		place(bp, asize);
+		return bp;
+	}
+
+	/* No fit found. Get more memory and place the block */
+	extendsize = MAX(asize, CHUNKSIZE);
+	if ((bp = extend_heap(extendsize / WSIZE)) == NULL)
+	{
+		return NULL;
+	}
+	place(bp, asize);
+
+	return bp;
+}
 
 
 
