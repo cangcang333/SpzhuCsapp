b38d989ea35c05be5b2e14d48edad9b977da6d0e
朱少鹏 <cangcang333@163.com>
2018-12-06 05:41:15 -0800

    add chapter optimization

---
+++ opt/vec.c
@@ -0,0 +1,51 @@
+#include "csapp.h"
+#include "vec.h"
+
+/* Create vector of specified length */
+vec_ptr new_vec(long int len)
+{
+	/* Allocate header structure */
+	vec_ptr result = (vec_ptr)malloc(sizeof(vec_rec));
+	if (!result)
+	{
+		return NULL;  /* Couldn't allocate storage */
+	}
+	result->len = len;
+	/* Allocate array */
+	if (len > 0)
+	{
+		data_t *data = (data_t *)calloc(len, sizeof(data_t));
+		if (!data)
+		{
+			free((void *) result);
+			return NULL;
+		}
+		result->data = data;
+	}
+	else
+	{
+		result->data = NULL;
+	}
+
+	return result;
+}
+
+/* 
+ * Retrieve vector element and store at dest.
+ * Return 0 (out of bounds) or 1 (successful) 
+ */
+int get_vec_element(vec_ptr v, long int index, data_t *dest)
+{
+	if (index < 0 || index >= v->len)
+	{
+		return 0;
+	}
+	*dest = v->data[index];
+	return 1;
+}
+
+/* Return length of vector */
+long int vec_length(vec_ptr v)
+{
+	return v->len;
+}
---
+++ opt/vec.h
@@ -0,0 +1,9 @@
+
+
+
+/* Create abstract data type for vector */
+typedef struct
+{
+	long int len;
+	data_t *data;
+} vec_rec, *vec_ptr
